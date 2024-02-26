29ad36d3397d4afc0e6edc79ce49e19975cf1854
VasilDogadin <Vaso-163@yandex.ru>
2024-02-26 18:48:50 +0400

    Заменил код на аннотации Lombok для класса City

--- src/main/java/com/javaacademy/City.java
+++ src/main/java/com/javaacademy/City.java
@@ -1,19 +1,21 @@
 package com.javaacademy;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 /**
  * Город
  */
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@ToString(exclude = {"peopleCount"})
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class City {
+    @NonNull
     public final String name;
+    @Setter
     public int peopleCount;
-
-    public City(String name, Integer peopleCount) {
-        if (name == null) {
-            throw new NullPointerException("name is marked non-null, but is null");
-        }
-        this.name = name;
-        this.peopleCount = peopleCount;
-    }
 
     public Garbage[] createGarbage() {
         return new Garbage[] {
@@ -21,25 +23,5 @@ public class City {
             new Garbage(GarbageType.PAPER, 15000, this.name),
             new Garbage(GarbageType.MIXED, 200000, this.name),
         };
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPeopleCount() {
-        return peopleCount;
-    }
-
-    public void setPeopleCount(Integer peopleCount) {
-        this.peopleCount = peopleCount;
-    }
-
-    //Внимание, только город выводится на экран
-    @Override
-    public String toString() {
-        return "City{" +
-                "name='" + name + '\'' +
-                '}';
     }
 }
