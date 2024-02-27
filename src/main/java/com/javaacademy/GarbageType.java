5675912340384db76070feceb08783b288434b29
Vital <vitalpasichnik@mai.ru>
2024-02-27 13:19:18 +0700

    add @Getter,NonNull,AllArgs

--- src/main/java/com/javaacademy/GarbageType.java
+++ src/main/java/com/javaacademy/GarbageType.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 import lombok.NonNull;
 
 @AllArgsConstructor
-@Getter
 public enum GarbageType {
     PAPER("Бумага"), GLASS("Стекло"), MIXED("Смешанный мусор");
+
     @NonNull
+    @Getter
     private final String value;
 }
