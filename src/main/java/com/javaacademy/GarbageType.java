5e4692d72271a3e307d7386479bed9734d71cd13
Vital <vitalpasichnik@mai.ru>
2024-02-27 14:45:17 +0700

    FieldsDefaults добавил

--- src/main/java/com/javaacademy/GarbageType.java
+++ src/main/java/com/javaacademy/GarbageType.java
@@ -1,14 +1,17 @@
 package com.javaacademy;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
 
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public enum GarbageType {
     PAPER("Бумага"), GLASS("Стекло"), MIXED("Смешанный мусор");
 
     @NonNull
     @Getter
-    private final String value;
+    String value;
 }
